6b8b7c5203d5cf4e28b7b23a3ee151947d9eacf9
leftinant <benjaminyoungner@gmail.com>
2025-07-19 14:28:08 +0300

    Preparing for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,12 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // <-- your Express backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
