259d497f41d5b83b431c329b7fa7920d385c0d39
leftinant <benjaminyoungner@gmail.com>
2025-07-22 10:47:49 +0300

    new commit

--- client/src/pages/PostForm.jsx
+++ client/src/pages/PostForm.jsx
@@ -49,14 +49,14 @@ export default function PostForm() {
 
     try {
       if (id) {
-        await axios.put(`${base}/api/posts/${id}`, formData, {
+        await axios.put(`${base}/posts/${id}`, formData, {
           headers: {
             "Content-Type": "multipart/form-data",
             Authorization: `Bearer ${token}`,
           },
         });
       } else {
-        await axios.post(`${base}/`, formData, {
+        await axios.post(`${base}/posts`, formData, {
           headers: {
             "Content-Type": "multipart/form-data",
             Authorization: `Bearer ${token}`,
